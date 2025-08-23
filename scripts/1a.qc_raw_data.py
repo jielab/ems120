34c8d5c0e7882115f10281fcde3e9d4b02f679c4
Jie Huang <jiehuang001@gmail.com>
2025-08-23 12:15:27 +0800

    JH update

--- scripts/1a.qc_raw_data.py
+++ scripts/1a.qc_raw_data.py
@@ -33,26 +33,6 @@ def add_time(df):
 
     return df
 
-# 电话评分函数
-def phone_scoring(phone):
-    phone = phone[3:]
-    n_4 = phone.count('4')
-    n_6 = phone.count('6')
-    n_8 = phone.count('8')
-    n_9 = phone.count('9')
-    if n_4 > 0:
-        score = 0
-    else:
-        score = n_8 + n_9 + n_6 * 0.5
-
-    return score
-
-# 添加电话分数
-def add_phone_score(df):
-    df['phone_score'] = df['联系电话'].apply(lambda x: phone_scoring(str(x)))
-
-    return df
-
 
 #############################################主函数######################################################################
 if __name__ == "__main__":
@@ -92,29 +72,7 @@ if __name__ == "__main__":
         except:
             continue_
         print('完成时间差添加')
-
-
-    # 添加手机号分数
-    if 'phone_score' in df.columns:
-        print('数据已包含手机号评分')
-    else:
-        print('开始手机号评分')
-        df = add_phone_score(df)
-        print('完成手机号评分')
-
-
-    ''''# 保存过程数据
-    df.to_excel(filepath)
-    # 添加街道分类
-    if '所属街道' in df.columns:
-        print('数据已包含所属街道')
-    else:
-        if len(df[df['接车地址经度'] == 0]) == 0:
-            print('开始进行街道分类')
-            df = add_street(df)
-            print('完成街道分类')
-        else:
-            print("请先完成经纬度添加再进行街道分类")'''
+        
 
     df.to_excel(outputpath + '/processed_' + filepath.split('/')[-1])
     print('完成数据QC')
